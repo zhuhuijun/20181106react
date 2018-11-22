e0515a3ac679e44d82652fb13cd8686e03c079f3
zhuhj <zhuhjtcx@qq.com>
2018-11-22 08:52:52 +0800

    lessons

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
-
+import  bg from '../../images/login-bg.png';
+import  avatar from '../../images/avatar.png';
+import './index.less';
 export default class Profile extends Component {
     render() {
         return (
-            <div className="profile">Profile</div>
+            <div className="profile">
+                <div className='profile-bg'>
+                    <img src={bg} alt=""/>
+                    <div className='avatar'>
+                        <img src={avatar} alt=""/>
+                    </div>
+                    <a href="" className='btn'>登录</a>
+                </div>
+            </div>
         )
     }
 }
